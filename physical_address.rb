a1367e4d442b2552c19817530bc09628d15b68bd
jin <jin@crypt.sg>
2014-10-26 12:53:03 +0800

    Renamed #split to #partition for a more consistent naming

--- physical_address.rb
+++ physical_address.rb
@@ -4,14 +4,14 @@ class PhysicalAddress < Address
 
   def initialize(address)
     @address = address
-    @frame, @offset = (is_resident? @address) ? split(@address) : [-1, nil]
+    @frame, @offset = (is_resident? @address) ? partition(@address) : [-1, nil]
   end
 
   def is_resident?(address)
     address.to_i >= 0 
   end
 
-  def split(address)
+  def partition(address)
     offset = address & 511
     frame = (address >> 9) & 1023 
     [frame, offset]

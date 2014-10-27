8430fab2815f3ae960f954eb362192b319209e31
jin <jin@crypt.sg>
2014-10-27 22:57:02 +0800

    test.sh now contains two tests; one with TLB enabled and one without.

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ruby app.rb --config test_files/initialization.txt --input test_files/input-vaddrs.txt --enable-tlb
+ruby app.rb --config test_files/initialization.txt --input test_files/input-vaddrs.txt > test_files/A0111764L_1.txt 
+ruby app.rb --config test_files/initialization.txt --input test_files/input-vaddrs.txt --enable-tlb > test_files/A0111764L_2.txt

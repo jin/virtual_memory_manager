75bf716a2a547b7b99a58bdd41e1f111aac80d35
jin <jin@crypt.sg>
2014-10-30 15:54:22 +0800

    Added new test cases

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ruby app.rb --config test_files/initialization.txt --input test_files/input-vaddrs.txt > test_files/A0111764L_1.txt 
 ruby app.rb --config test_files/initialization.txt --input test_files/input-vaddrs.txt --enable-tlb > test_files/A0111764L_2.txt
+
+# ruby app.rb --config test_files/jermynInput1.txt --input test_files/jermynInput2.txt > test_files/A0111764L_1.txt 
+# ruby app.rb --config test_files/jermynInput1.txt --input test_files/jermynInput2.txt --enable-tlb > test_files/A0111764L_2.txt
